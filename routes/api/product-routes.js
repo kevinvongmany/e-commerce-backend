6f021c4c99b1c7a5c0ffca973f93f8f684173f8f
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-09-01 21:39:16 +1000

    Updated product route to validate if product id exists

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -21,6 +21,10 @@ router.get('/:id', async (req, res) => {
     const product = await Product.findByPk(req.params.id, {
       include: [{ model: Category }, { model: Tag }],
     });
+    if (!product) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
     res.status(200).json(product);
   } catch (err) {
     res.status(500).json(err);
